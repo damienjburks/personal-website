5eb29e5ccce102f6c3512718f0f0c951fd5b9db4
Damien Burks <damien@damienjburks.com>
2025-12-08 21:59:11 -0600

    updating

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 module "website" {
   source  = "damienjburks/secure-static-site/aws"
   version = "1.0.4"
- 
+
   bucket_name             = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
   enable_domain           = true
   logging_enabled         = true
   enable_failover         = true
   enable_replication      = true
   enable_security_headers = true
-  create_route53_zone = true
+  create_route53_zone     = true
   primary_region          = var.primary_region
   failover_region         = var.failover_region
 
