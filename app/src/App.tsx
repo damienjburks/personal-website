aae706247941e32c31c5885573a0ad2abb4f5d72
Damien Burks <damien@damienjburks.com>
2025-12-09 23:50:31 -0600

    Commenting out youtube videos for now

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           <About />
           <Companies />
           <Specialties />
-          <YouTubeVideos />
+          {/* <YouTubeVideos /> */}
           <Speaking />
           <Courses />
           <Contact />
