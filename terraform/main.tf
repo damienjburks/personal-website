0d080beebe4b39fcb413c6a3d43cf7156c4a26bc
Damien Burks <damien@damienjburks.com>
2025-12-13 10:15:35 -0600

    updating module with other records for route53

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "website" {
   source  = "damienjburks/secure-static-site/aws"
-  version = "1.0.10"
+  version = "1.1.0"
 
   bucket_name             = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
   enable_domain           = true
@@ -18,4 +18,41 @@ module "website" {
     Environment = "Production"
     ManagedBy   = "Terraform Cloud"
   }
+}
+
+# Blog Endpoint
+resource "aws_route53_record" "blog_a" {
+  zone_id = module.website.route53_zone_id
+  name    = "blog.damienjburks.com"
+  type    = "A"
+  ttl     = 300
+  records = [
+    "76.76.21.21"
+  ]
+}
+
+# Google Workspace MX records (for email)
+resource "aws_route53_record" "mx" {
+  zone_id = module.website.route53_zone_id
+  name    = "damienjburks.com"
+  type    = "MX"
+  ttl     = 300
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 alt3.aspmx.l.google.com",
+    "10 alt4.aspmx.l.google.com",
+  ]
+}
+
+# Google site verification TXT record
+resource "aws_route53_record" "txt_verification" {
+  zone_id = module.website.route53_zone_id
+  name    = "damienjburks.com"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "v=spf1 include:_spf.google.com include:_spf-us.ionos.com ~all"
+  ]
 }
